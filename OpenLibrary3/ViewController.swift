5c69536398a9fb6b676feda415e32d6ec3a2462e
Juan Esteban Diaz <juesdimo@gmail.com>
2018-02-25 14:23:08 -0500

    Initial Commit

---
+++ OpenLibrary3/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  OpenLibrary3
+//
+//  Created by Juan Esteban Diaz Montejo on 25/02/18.
+//  Copyright © 2018 Juan Esteban Diaz Montejo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
